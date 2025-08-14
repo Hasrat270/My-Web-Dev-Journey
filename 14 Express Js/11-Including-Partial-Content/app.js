e0628f71ceda80b1e73244653dbfe2ce2e0e3ad5
Hasrat Afridi <hasrat3701@gmail.com>
2025-08-15 02:35:19 +0500

    Version-0.32

---
+++ 14 Express Js/11-Including-Partial-Content/app.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (request, response) => {
+  response.render("index");
+});
+app.get("/restaurants", (req, res) => {
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+  
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
+});
+
+app.get("/about", (request, response) => {
+  response.render("about");
+});
+app.get("/recommend", (request, response) => {
+  response.render("recommend");
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
+});
+
+app.get("/confirm", (request, response) => {
+  response.render("confirm");
+});
+
+app.listen(3000, () => console.log("Server Listening on port 3000!"));
