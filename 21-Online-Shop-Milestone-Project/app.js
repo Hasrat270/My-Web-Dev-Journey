d6d74ccdd2ede653d5d7457dbde3f8d9cad691c5
“Hasrat <hasrat3701@gmail.com>
2025-10-09 00:42:01 +0500

    Converted 21-Online-Shop-Milestone-Project to normal folder

---
+++ 21-Online-Shop-Milestone-Project/app.js
@@ -0,0 +1,67 @@
+const path = require("path");
+
+const express = require("express");
+const csrf = require("csurf");
+const expressSession = require("express-session");
+
+const createSessionConfig = require("./config/session");
+const db = require("./data/database");
+const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+const checkAuthStatusMiddleware = require("./middlewares/check-auth");
+const protectRoutesMiddleware = require("./middlewares/protect-routes");
+const cartMiddleware = require("./middlewares/cart");
+const updateCartPricesMiddleware = require("./middlewares/update-cart-prices");
+const notFoundMiddleware = require("./middlewares/not-found");
+const authRoutes = require("./routes/auth.routes");
+const productRoutes = require("./routes/products.routes");
+const baseRoutes = require("./routes/base.routes");
+const adminRoutes = require("./routes/admin.routes");
+const cartRoutes = require("./routes/cart.routes");
+const ordersRoutes = require("./routes/orders.routes");
+
+const app = express();
+
+app.set("view engine", "ejs"); // setting ejs
+app.set("views", path.join(__dirname, "views")); // engine
+
+app.use(express.static("public")); // access static files
+app.use("/products/assets", express.static("product-data"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+app.use(csrf());
+
+app.use(cartMiddleware); // Cart Middleware
+app.use(updateCartPricesMiddleware);
+
+app.use(addCsrfTokenMiddleware);
+app.use(checkAuthStatusMiddleware);
+
+app.use(baseRoutes);
+app.use(authRoutes);
+app.use(productRoutes);
+app.use("/cart", cartRoutes);
+// It will protect that routes
+// which are coming next in line , then if we are logged out we can
+// not access a route just by entering it in the url tab !
+app.use("/orders", protectRoutesMiddleware, ordersRoutes);
+app.use("/admin", protectRoutesMiddleware, adminRoutes);
+
+app.use(notFoundMiddleware);
+
+app.use(errorHandlerMiddleware);
+
+db.connectToDatabase()
+  .then(function () {
+    app.listen(3000, function () {
+      console.log("Server Listening on : http://localhost:3000");
+    });
+  })
+  .catch(function (error) {
+    console.log("Failed to connect to database!");
+    console.log(error);
+  });
